65b7a2dfe7113a787a1b11e70b70893e6ba74da6
andy <ritmorica@gmail.com>
2023-11-10 14:45:41 +0300

    fix: add new data

--- main.py
+++ main.py
@@ -3,30 +3,47 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 import redis
+from dotenv import load_dotenv
+import os
 
 class WriteDataRequest(BaseModel):
     phone: str
     address: str
 
-tags_metadata = [
-    {
-        "name": "Health",
-        "description": "Стартовая точка"
-    }
-]
-app = FastAPI(openapi_url="/api/v1/openapi.json", docs_url='/swagger', openapi_tags=tags_metadata)
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+app = FastAPI(openapi_url="/api/v1/openapi.json", docs_url='/swagger')
 
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
-@app.post("/write_data")
+host = os.environ.get("host")
+port = os.environ.get("port")
+db_name = os.environ.get("db_name")
+db_pass = os.environ.get("db_pass")
+
+redis_client = redis.StrictRedis(
+    host=host,
+    port=port,
+    db=db_name,
+    password=db_pass,
+    decode_responses=True)
+
+@app.post("/write_data", tags=['Data'])
 def write_data(request: WriteDataRequest):
+    """API для записи данных в БД (Телефон, Адрес)"""
     phone = request.phone
     address = request.address
-    redis_client.set(phone, address)
-    return JSONResponse(content={"message": "Data written successfully"})
+    existing_address = redis_client.get(phone)
+    if existing_address:
+        redis_client.set(phone, address)
+        return JSONResponse(content={"message": "Обновляем данные"})
+    else:
+        # Write new data
+        redis_client.set(phone, address)
+        return JSONResponse(content={"message": "Вносим новую запись"})
 
-@app.get("/check_data")
+@app.get("/check_data", tags=['Data'])
 def check_data(phone: str):
+    """API для извлечения данных из БД согласно указанному телефону"""
     address = redis_client.get(phone)
     if address:
         return JSONResponse(content={"address": address})
